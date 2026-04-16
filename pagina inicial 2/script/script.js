534f1abb7e1662624ee12edbcb6e7e385a610239
GuilhermeBronze <gui.broleze@gmail.com>
2026-04-15 21:56:19 -0300

    glob

---
+++ pagina inicial 2/script/script.js
@@ -0,0 +1,19 @@
+function glob(){
+
+    let jumscare = document.querySelector("body#jumscare")
+    const audio = new Audio("media/globglab.mp3")
+
+    audio.play()
+        window.alert("Glob Glab")
+
+        
+        jumscare.style.backgroundImage = "url(style/jumscare.png)";
+        
+        
+    }
+    
+
+
+    
+
+
