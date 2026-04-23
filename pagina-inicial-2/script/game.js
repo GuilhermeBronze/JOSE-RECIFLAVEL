f64b7207ce5e31725e22b61e082339a45041a15f
PedrinSens <pedroantoniclaudino@gmail.com>
2026-04-22 21:12:08 -0300

    bilola

--- pagina-inicial-2/script/game.js
+++ pagina-inicial-2/script/game.js
@@ -1,4 +1,30 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Pagina carregada!");
-});
+let money = {
+    p1: 0,
+    p2: 0,
+    p3: 0,
+    p4: 0,
+    p5: 0,
+    p6: 0
+}
 
+function addMoney(button){
+    let painel = button.parentElement
+    let id = painel.id
+    let input = painel.querySelector('input[type="text"]')
+    let valor = Number(input.value.replace(/\D/g, '')) / 100
+    money[id] = money[id] + valor
+
+    let pMoney = painel.querySelector('p:nth-of-type(2)')
+    pMoney.textContent = "Saldo: R$ " + money[id].toFixed(2).replace(".", ",")
+}
+
+function subMoney(button){
+    let painel = button.parentElement
+    let id = painel.id
+    let input = painel.querySelector('input[type="text"]')
+    let valor = Number(input.value.replace(/\D/g, '')) / 100
+    money[id] = money[id] - valor
+
+    let pMoney = painel.querySelector('p:nth-of-type(2)')
+    pMoney.textContent = "Saldo: R$ " + money[id].toFixed(2).replace(".", ",")
+}
